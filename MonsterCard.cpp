2e970058268526a94578d4ff27a12716ae133d62
radintsochev <115207824+radintsochev@users.noreply.github.com>
2023-10-13 13:11:40 +0300

    Delete MonsterCard.cpp

--- MonsterCard.cpp
+++
@@ -1,9 +0,0 @@
-#include "MonsterCard.h"
-#include <cstring>
-#pragma warning(diasble : 4996)
-MonsterCard::MonsterCard(const char* name, const unsigned pointsATK, const unsigned pointsDEF) {
-	strcpy(this->name, name);
-	this->pointsATK = pointsATK;
-	this->pointsDEF = pointsDEF;
-
-}
